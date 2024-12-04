b119e84da03c3cb16587cb7334de1aba410b8e4b
bammite_prince <bammite_prince>
2024-12-04 19:55:48 +0000

    f

--- js/data.js
+++ js/data.js
@@ -1,13 +1,13 @@
 const catalogue = [
-    { id: 1, name: "Croissant", price: 1000, category: "PATISSERIE", description: "Un classique de la pâtisserie française...", image: "../assets/croissant.jpeg" },
-    { id: 2, name: "Éclair au Chocolat", price: 1200, category: "PATISSERIE", description: "Délicieux et gourmand...", image: "../assets/eclairs-au-chocolat-fond-blanc-generes-par-ia_985692-317.avif" },
-    { id: 3, name: "Pancake", price: 800, category: "PATISSERIE", description: "Délicieux et gourmand...", image: "../assets/pancakes-baies-du-sirop-erable-isoles-fond-blanc_170984-19923.jpeg" },
-    { id: 4, name: "Gateau Personnalisable", price: 5000, category: "PATISSERIE", description: "Délicieux et gourmand...", image: "../assets/gateau.jpg" },
-    { id: 5, name: "Crepes", price: 900, category: "PATISSERIE", description: "Délicieux et gourmand...", image: "../assets/crepes-francaises-isolees-fond-blanc-uni_146508-7012.avif" },
-    { id: 6, name: "Brochette-Bœuf", price: 3000, category: "GRIADE", description: "Brochette savoureuse...", image: "../assets/brochette-viande-legumes-isole-fond-blanc-ai-generative_123827-24960.jpeg" },
-    { id: 7, name: "Burger Américain", price: 2000, category: "GRIADE", description: "Un burger généreux et savoureux...", image: "../assets/order-3.png" },
-    { id: 8, name: "Poulet Braisé", price: 3500, category: "GRIADE", description: "Poulet braisé au goût unique...", image: "../assets/poulet_Braise.jpeg" },
-    { id: 9, name: "Tacos Mexicains", price: 1800, category: "GRIADE", description: "Tacos aux saveurs épicées...", image: "../assets/taco-mexicain-isole-fond-blanc_198067-100.jpeg" }
+    { id: 1, name: "Croissant", price: 1000, category: "PATISSERIE", description: "Un classique de la pâtisserie française...", image: "./../assets/croissant.jpeg" },
+    { id: 2, name: "Éclair au Chocolat", price: 1200, category: "PATISSERIE", description: "Délicieux et gourmand...", image: "./../assets/eclairs-au-chocolat-fond-blanc-generes-par-ia_985692-317.avif" },
+    { id: 3, name: "Pancake", price: 800, category: "PATISSERIE", description: "Délicieux et gourmand...", image: "./../assets/pancakes-baies-du-sirop-erable-isoles-fond-blanc_170984-19923.jpeg" },
+    { id: 4, name: "Gateau Personnalisable", price: 5000, category: "PATISSERIE", description: "Délicieux et gourmand...", image: "./../assets/gateau.jpg" },
+    { id: 5, name: "Crepes", price: 900, category: "PATISSERIE", description: "Délicieux et gourmand...", image: "./../assets/crepes-francaises-isolees-fond-blanc-uni_146508-7012.avif" },
+    { id: 6, name: "Brochette-Bœuf", price: 3000, category: "GRIADE", description: "Brochette savoureuse...", image: "./../assets/brochette-viande-legumes-isole-fond-blanc-ai-generative_123827-24960.jpeg" },
+    { id: 7, name: "Burger Américain", price: 2000, category: "GRIADE", description: "Un burger généreux et savoureux...", image: "./../assets/order-3.png" },
+    { id: 8, name: "Poulet Braisé", price: 3500, category: "GRIADE", description: "Poulet braisé au goût unique...", image: "./../assets/poulet_Braise.jpeg" },
+    { id: 9, name: "Tacos Mexicains", price: 1800, category: "GRIADE", description: "Tacos aux saveurs épicées...", image: "./../assets/taco-mexicain-isole-fond-blanc_198067-100.jpeg" }
 ];
 
 const dishes = [
